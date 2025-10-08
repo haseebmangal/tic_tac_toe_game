86d1544007213543b1e75b06e964e883be02432e
haseebmangal <haseebmangal_33@yahoo.com>
2025-10-08 23:01:02 +0500

    upload the new work, the bug removed which caused to remain the box color even after reseting the game, winning and game draw

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let msg = document.querySelector("#msg");
 
 // a variable to track the alternate turns
 let turnO = true; // PlayerX, PlayerO
+let count = 0; // to track the number of turns played
 
 // Making array of winiing combinations
 const winPtrns = [
@@ -20,11 +21,14 @@ const winPtrns = [
     [6, 7, 8],
 ];
 
+// function to reset the game
 const resetGame = () => {
     turnO = true;
     count = 0;
     enableBoxes();
     msgContainer.classList.add("hide");  
+    resetBoard();
+
 }
 
 // Checking each elements of array through "forEach", 
@@ -33,10 +37,16 @@ boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if (turnO) { // true means Player O's turn
             box.innerText = "O";
-            turnO = false;
+            box.classList.add('mark-o');
+            box.style.backgroundColor = '#cfe8ff';
+            box.style.color = "#003a6b";
+            
         } else { // false means Player X's turn
             box.innerText = "X";
-            turnO = true;
+            box.classList.add('mark-x');
+            box.style.backgroundColor = '#ffd9d9';
+            box.style.color = "#7a0000";
+            
         }
         box.disabled = true;
         count++;
@@ -44,22 +54,46 @@ boxes.forEach((box) => {
         let isWinner = checkWin();
         if (count === 9 && !isWinner) {
            gameDraw();
+           resetBoard();
         }
+        turnO = !turnO;
     });
     })
 
+    function resetBoard() {
+    boxes.forEach(box => {
+    // Remove any classes if you added them elsewhere
+    box.classList.remove('mark-o', 'mark-x', 'win');
+
+    // Clear all inline styles that affect appearance
+    box.style.backgroundColor = '';
+    box.style.color = '';
+    box.style.outline = '';
+    box.style.border = '';
+
+    // Reset content & state
+    box.innerText = '';
+    box.disabled = false;
+    })};
+    count = 0;
+    turnO = true;
+
+// function to handle game draw
 const gameDraw = () => {
    msg.innerText = "Game was a Draw!";
    msgContainer.classList.remove("hide");
    disableBoxes();
+   resetBoard()
 ;}
 
+// function to disable boxes
 const disableBoxes = () => {
     for(let box of boxes) {
         box.disabled = true;
     }
 };
 
+// function to enable boxes
 const enableBoxes = () => {
     for (let box of boxes) {
         box.disabled = false;
@@ -67,11 +101,14 @@ const enableBoxes = () => {
     };
 };
 
+// function to show the winner
 const showWinner = (winner) => {
     msg.innerText = `Congratulation, Player ${winner} has won!`;
     msgContainer.classList.remove("hide");
     disableBoxes();
+    resetBoard();
 }
+// function to check the winning combinations
 const checkWin = () => {
     for (let pattern of winPtrns) {
         let pos1val = boxes[pattern[0]].innerText;
@@ -80,13 +117,15 @@ const checkWin = () => {
 
         if (pos1val != "" && pos2val != "" && pos3val != "") {
             if (pos1val === pos2val && pos2val === pos3val) {
-                console.log("Winner");
                 showWinner(pos1val);
             }
         }  
     }
 
 }
+
+// adding event listener to reset button
 newGameBtn.addEventListener("click", resetGame);
+
 resetBtn.addEventListener("click", resetGame )
 
